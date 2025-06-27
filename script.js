3da7d60b57d680e3c770f1f232e2a946fc1b4fa8
ZhirDev <zheerjalal22@gmail.com>
2025-06-27 19:42:16 +0300

    zyadkrdni + and - boi Barham zyad bbe

--- script.js
+++ script.js
@@ -3,3 +3,21 @@
     const menu = document.getElementById('menu');
     menu.classList.toggle('hidden');
   });
+
+let count = 0;
+
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('subtract');
+const countDisplay = document.getElementById('count');
+
+
+
+addBtn.addEventListener('click', () => {
+  count++;
+  countDisplay.textContent = count;
+});
+
+subBtn.addEventListener('click', () => {
+  count--;
+  countDisplay.textContent = count;
+});
